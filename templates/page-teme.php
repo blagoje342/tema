db4e6f160589c3bc3082d8954254a710d24b2ae0
milentije <mile_iz_back@hotmail.com>
2013-01-10 18:59:21 +0100

    petlja za naslovnu

--- templates/page-teme.php
+++ templates/page-teme.php
@@ -1,56 +1,23 @@
     <div class="container">
       <div class="row">
        <div class"span12">
-    
+   
+
+<?php $teme_turizma = array('Priroda', 'Kulinarstvo', 'Kultura', 'Aktivnosti'); ?> 
+<?php foreach ($teme_turizma as $value) { ?> 
         <div class="span3">
 	   <?php if(is_front_page()) {                
-		$post_id = 237; // assign post id
-                $queried_post = get_page($post_id); ?>
+		$post = get_page_by_title($value); // assign post id
+                $queried_post = get_page($post); ?>
 
-		<div class='tema thumbnail'><a href="<?php echo get_page_link($post_id); ?>"><?php echo get_the_post_thumbnail( $post_id, 'medium'); ?></a></div>
-		<div class='product_title'><h2><a href="<?php echo get_page_link($post_id); ?>"><?php echo $queried_post->post_title;  ?></a></h2></div>
+		<div class='tema thumbnail'><a href="<?php echo get_page_link($post->ID); ?>"><?php echo get_the_post_thumbnail( $post->ID, 'medium'); ?></a></div>
+		<div class='product_title'><h2><a href="<?php echo get_page_link($post->ID); ?>"><?php echo $queried_post->post_title;  ?></a></h2></div>
                 <div class='description_product'>
-                <?php echo $queried_post->post_excerpt;  ?></div>
-            <?php  } ?>
+                <?php echo $queried_post->post_content;  ?></div>
+            <?php  }?>
                </div>
 
-
-        <div class="span3">
-	   <?php if(is_front_page()) {                
-		$post_id = 237; // assign post id
-                $queried_post = get_page($post_id); ?>
-
-		<div class='tema thumbnail'><a href="<?php echo get_page_link($post_id); ?>"><?php echo get_the_post_thumbnail( $post_id, 'medium'); ?></a></div>
-		<div class='product_title'><h2><a href="<?php echo get_page_link($post_id); ?>"><?php echo $queried_post->post_title;  ?></a></h2></div>
-                <div class='description_product'>
-                <?php echo $queried_post->post_excerpt;  ?></div>
-            <?php  } ?>
-               </div>
-
-
-        <div class="span3">
-	   <?php if(is_front_page()) {                
-		$post_id = 237; // assign post id
-                $queried_post = get_page($post_id); ?>
-
-		<div class='tema thumbnail'><a href="<?php echo get_page_link($post_id); ?>"><?php echo get_the_post_thumbnail( $post_id, 'medium'); ?></a></div>
-		<div class='product_title'><h2><a href="<?php echo get_page_link($post_id); ?>"><?php echo $queried_post->post_title;  ?></a></h2></div>
-                <div class='description_product'>
-                <?php echo $queried_post->post_excerpt;  ?></div>
-            <?php  } ?>
-               </div>
-
-       <div class="span3">
-	   <?php if(is_front_page()) {                
-		$post_id = 237; // assign post id
-                $queried_post = get_page($post_id); ?>
-
-		<div class='tema thumbnail'><a href="<?php echo get_page_link($post_id); ?>"><?php echo get_the_post_thumbnail( $post_id, 'medium'); ?></a></div>
-		<div class='product_title'><h2><a href="<?php echo get_page_link($post_id); ?>"><?php echo $queried_post->post_title;  ?></a></h2></div>
-                <div class='description_product'>
-                <?php echo $queried_post->post_excerpt;  ?></div>
-            <?php  } ?>
-               </div>
+            <?php  }?>
 </div>
 </div>
 </div>
